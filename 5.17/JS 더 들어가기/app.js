bde754bb2204acb8188ba0ed415f312e7f79135f
QUOTIENT <tjsgh5965@naver.com>
2022-05-17 15:10:30 +0900

    final

--- 5.17/JS 더 들어가기/app.js
+++ 5.17/JS 더 들어가기/app.js
@@ -1,1 +1,24 @@
-const writer = '천예지';
+let id;
+
+const menu_login = document.querySelector('#login-link');
+
+function loginAlert() {
+    id = prompt("ID를 입력하세요.");
+    console.log(`id는 ${id}입니다.`);
+}
+
+function askId() {
+    
+    if (menu_login.style.color === "blue") {
+        menu_login.style.color = "red";
+    }
+    else {
+        menu_login.style.color = "blue";
+    }
+
+    // const user_id = prompt("ID값을 입력하세요");
+
+    // menu_login.innerHTML = user_id;
+}
+
+menu_login.addEventListener('click', askId);
